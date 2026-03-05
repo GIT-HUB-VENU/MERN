e97717f0fe9a2d5fd122ecde999e552bc7774093
GIT-HUB-VENU <venugopal5880844@gmail.com>
2026-03-05 15:41:27 +0530

    updated

--- part-4/page34.js
+++ part-4/page34.js
@@ -1,3 +1,13 @@
 import express from "express";
 const app = express();
+app.set("view engine","ejs");
+app.set("views","views");
 app.listen(8080,()=>{console.log("Server started at 8080")});
+
+app.get("/login",(req,res)=>{
+    res.render("login");
+});
+
+app.get("/register",(req,res)=>{
+    res.render("register");
+});
