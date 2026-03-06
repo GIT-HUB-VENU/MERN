f36ee9dbebc691593d726166423794bf9eaeec97
GIT-HUB-VENU <venugopal5880844@gmail.com>
2026-03-06 12:00:36 +0530

    updated

--- part-4/page34.js
+++ part-4/page34.js
@@ -3,13 +3,25 @@ const app = express();
 app.set("view engine","ejs");
 app.set("views","views");
 app.listen(8080,()=>{console.log("Server started at 8080")});
-
+app.use(express.urlencoded({extended:true}));
 const users = [{id: 1, username: "shetty", email: "shetty@gmail.com", password: "password123"},
 {id: 2, username: "kannu", email: "kannu@gmail.com", password: "password456"},
 {id: 3, username: "somu", email: "somu@gmail.com", password: "password789"}];
 
 app.get("/login",(req,res)=>{
     res.render("login");
+});
+
+app.post("/login",(req,res)=>{
+    const { email, password } = req.body;
+    const user = users.find(u => u.email === email && u.password === password);
+    if (user) {
+        res.redirect("/");
+    }
+    else{
+        res.redirect("/login");
+    }
+    res.redirect("/");
 });
 
 app.get("/register",(req,res)=>{
